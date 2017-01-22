8ef1d415145f6aa664905c7643324abd1ee4d786
esdth <keepass.log@gmail.com>
2017-01-22 19:21:58 +0200

    Simple form data wiring with controller

--- provider/routes.py
+++ provider/routes.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/signup/")
+@app.route("/signup/", methods = ['POST', 'GET'])
 def index():
+    error = None
+    if request.method == 'POST':
+        return request.form['login']
     return render_template("index.html")
-
 
