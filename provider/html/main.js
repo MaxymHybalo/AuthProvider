642a2f1b2905a4da03f95cd235615c1db29c1695
Vadim Perenesenko <smeptb15@gmail.com>
2017-01-22 18:11:44 +0200

    Create html-page with registration

---
+++ provider/html/main.js
@@ -0,0 +1,10 @@
+$('.ui.form')
+    .form({
+        fields: {
+            last     : 'empty',
+            first   : 'empty',
+            email : 'empty',
+            number : 'empty'
+        }
+    });
+
