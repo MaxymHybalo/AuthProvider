53ae8fe91234836b6424a14b9217855723b92e57
esdth <keepass.log@gmail.com>
2017-01-26 15:18:34 +0200

    Updated database environment

--- provider/entities.py
+++ provider/entities.py
@@ -1,23 +1,21 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import declarative_base
+from sqlalchemy import Column, String, Integer
+from provider.database import Base
 
 
+class User(Base):
 
-db = SQLAlchemy()
-# Base = declarative_base()
-
-
-class User(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), unique=True, index=True,
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    login = Column(String(40), unique=True, index=True,
                          nullable=False)
-    # first_name = db.Column(db.String(40),)
+
+    first_name = Column(String(40), nullable=False)
 
     def to_string(self):
         return "{" \
                "\n\tid: " + str(self.id) + \
-               "\n\tlogin: " + self.username + \
+               "\n\tlogin: " + self.login +\
+               "\n\tfirst_name: " + self.first_name +\
                "\n}"  # to simplify user data output
 
 
--- provider/routes.py
+++ provider/routes.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from provider.entities import User
-from provider.entities import db
+from provider.database import db_session, init_db
 
 
 app = Flask(__name__)
 
 
-@app.route("/signup/", methods = ['POST', 'GET'])
+@app.route("/signup/", methods=['POST', 'GET'])
 def index():
     error = None
     if request.method == 'POST':
@@ -19,31 +19,26 @@ def index():
 
 @app.route("/showusers", methods=['GET'])
 def all_users():
-    users = db.session.query(User).all()
-    suser = ''
+    users = User.query.all()
+    all_users = ''
     for u in users:
-        suser += u.to_string() + "\n"
-    return suser
+        all_users += u.to_string() + "\n"
+    return all_users
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 if __name__ == '__main__':
 
     app.debug = True
     app.secret_key = 'development'
-    app.config.update({
-        'SQLALCHEMY_DATABASE_URI': 'sqlite:///users.db'
-    })
-    db.init_app(app)
-    db.app = app
-    db.create_all()
-
-    # user = User(username='esd1th')
-    # try:
-    #     db.session.add(user)
-    #     db.session.commit()
-    # except:
-    #     db.session.rollback()
-
-    # users = db.session.query(User).all()
-    # for u in users:
-    #     print(u.to_string())
+    init_db()
     app.run()
+    # try:
+    #     u = User(login="sS", first_name="Sd")
+    #     db_session.add(u)
+    #     db_session.commit()
+    # except:
+    #     db_session.rollback() method to write into database
