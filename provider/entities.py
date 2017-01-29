aace162b6cf97e1b283bf35e03b9f99314c21580
esdth <keepass.log@gmail.com>
2017-01-29 16:53:22 +0200

    [Backend] Added Client entity

--- provider/entities.py
+++ provider/entities.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, \
+    ForeignKey, Boolean, Text
 from provider.database import Base
 
 
@@ -7,7 +8,7 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     login = Column(String(20), unique=True, index=True,
-                         nullable=False)
+                   nullable=False)
     password = Column(String(30), nullable=False)
     first_name = Column(String(30), nullable=False)
     last_name = Column(String(30), nullable=False)
@@ -26,6 +27,45 @@ class User(Base):
                "\n}"  # to simplify user data output
 
 
+class Client(Base):
+
+    __tablename__ = 'clients'
+    # Readable client name
+    name = Column(String(30))
+
+    description = Column(String(300))
+
+    user_id = Column(ForeignKey('user.id'))
+    user = relationship('User')
+    client_id = Column(String(40), primary_key=True)
+    client_secret = Column(String(55), unique=True,
+                           nullable=False,
+                           index=True)
+    is_confidential = Column(Boolean)
+    _redirect_uris = Column(Text)
+    _default_scopes = Column(Text)
+
+    @property
+    def client_type(self):
+        if self.is_confidential:
+            return 'confidential'
+        return 'public'
+
+    @property
+    def redirect_uris(self):
+        if self._redirect_uris:
+            return self._redirect_uris.split()
+        return []
+
+    @property
+    def default_redirect_uri(self):
+        return self.redirect_uris[0];
+
+    @property
+    def default_scopes(self):
+        if self._default_scopes:
+            return self._default_scopes.split()
+        return []
 
 
 
