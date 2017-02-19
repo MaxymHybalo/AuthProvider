a05507ef5e737312ed2d57e893960132464cf605
esdth <keepass.log@gmail.com>
2017-02-19 19:12:59 +0200

    [Backend] Added client delete api's

--- provider/client_api/client_routes.py
+++ provider/client_api/client_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from provider.models.user import User
-from provider.models.client import Client, add_client, get_user_clients
+from provider.models.client import Client, add_client, get_user_clients, delete_client
 from provider.utils.database import db_session
 
 client_routes = Blueprint('client_routes', __name__)
@@ -9,9 +9,18 @@ client_routes = Blueprint('client_routes', __name__)
 @client_routes.route('/client/', methods=['POST', 'GET'])
 def client():
     if request.method == 'POST':
-        result = add_client(request.json)
-        return jsonify(message=result)
+        return jsonify(message=add_client(request.json))
     result = get_user_clients()
     if isinstance(result, list):
         return jsonify(result)
     return jsonify(error=result)
+
+
+@client_routes.route('/client/<string:client_id>/', methods=['DELETE'])
+def del_client(client_id):
+    result = delete_client(client_id)
+    if result[0]:
+        return jsonify(message=result[1])
+    return jsonify(error=result[1])
+
+
--- provider/models/client.py
+++ provider/models/client.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.exc import SQLAlchemyError
 from provider.utils.database import Base, db_session
 from provider.models.user import User, current_user
+from provider.models.token import Token
 from provider.utils.jwt_auth import token_expected
 from werkzeug.security import gen_salt
 
@@ -82,6 +83,37 @@ def add_client(json):
     if user:
         return write_to_database(json, user)
     return 'You are not authorized'
+
+
+def delete_client(id):
+    user = current_user()
+    if user:
+        client = Client.query.filter(Client.client_id == id).first()
+        if client.user_id == user.id:
+            result = _delete_cascade(client)
+            if result:
+                return True, 'Client successful deleted'
+    return False, 'Delete operation error, user or client id wrong'
+
+
+def _delete_cascade(client):
+    try:
+        _delete_client_tokens(client)
+        db_session.delete(client)
+        db_session.commit()
+    except SQLAlchemyError:
+        return False
+    return True
+
+
+def _delete_client_tokens(client):
+    try:
+        tokens = Token.query.filter(Token.client_id == client.client_id).all()
+        for t in tokens:
+            db_session.delete(t)
+        db_session.commit()
+    except SQLAlchemyError:
+        pass
 
 
 def get_user_clients():
